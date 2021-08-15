577e236051c8e3a29db8de1456b490dd8810b7b4
jeongho <qkrjh0904@gmail.com>
2021-08-16 04:15:40 +0900

    :sparkles: Order, OrderItem Entity 개발

--- src/main/java/com/jeongho/jpashop/domain/Order.java
+++ src/main/java/com/jeongho/jpashop/domain/Order.java
@@ -6,16 +6,19 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
@@ -36,19 +39,58 @@ public class Order {
     private OrderStatus status; // 주문상태 (ORDER, CANCEL)
 
     // 연관관계 편의 메소드
-    public void setMember(Member member){
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
 
-    public void addOrderItem(OrderItem orderItem){
+    public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    public void setDelivery(Delivery delivery){
+    public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
 
+    /**
+     * @desc : 생성 메소드
+     * @author : 박정호
+     * @date : 2021-08-16 오전 3:10
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        Arrays.stream(orderItems).forEach(order::addOrderItem);
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * @desc : 취소 비즈니스 로직
+     * @author : 박정호
+     * @date : 2021-08-16 오전 3:10
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        orderItems.forEach(OrderItem::cancel);
+    }
+
+    /**
+     * @desc : 전체 주문 가격 조회 비즈니스 로직
+     * @author : 박정호
+     * @date : 2021-08-16 오전 4:04
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 }
--- src/main/java/com/jeongho/jpashop/domain/OrderItem.java
+++ src/main/java/com/jeongho/jpashop/domain/OrderItem.java
@@ -26,4 +26,37 @@ public class OrderItem {
 
     private int orderPrice; // 주문가격
     private int count;      // 주문수량
+
+    /**
+    * @desc : 생성 메소드
+    * @author : 박정호
+    * @date : 2021-08-16 오전 4:12
+    */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    /** 
+    * @desc : 취소 비즈니스 로직
+    * @author : 박정호
+    * @date : 2021-08-16 오전 4:03
+    */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /**
+    * @desc : 총 가격
+    * @author : 박정호
+    * @date : 2021-08-16 오전 4:06
+    */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
